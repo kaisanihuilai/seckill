1fc95753e0d9e1328b2db4770134883f42054d0e
kaisanihuilai <2411797989@qq.com>
2018-08-01 17:04:57 +0800

    提交20180801

--- src/main/webapp/resources/script/seckill.js
+++ src/main/webapp/resources/script/seckill.js
@@ -3,7 +3,9 @@
 var seckill={
     //封装秒杀相关ajax的url
     URL:{
-
+        now:function () {
+            return "/seckill/time/now"
+        }
     },
     //详情页秒杀逻辑
     detail:{
@@ -11,9 +13,6 @@ var seckill={
         init:function (params) {
             //手机验证和登录
             var killPhone = $.cookie('killPhone');
-            var startTime=params['startTime'];
-            var endTime=params['endTime'];
-            var seckillId=params['seckillId'];
             if (!seckill.validatePhone(killPhone)){
                 //绑定手机号 控制输出
                 var killPhoneModal = $("#killPhoneModal");
@@ -32,6 +31,20 @@ var seckill={
                     }
                 })
             }
+            //已登录
+            //计时交互
+            var startTime=params['startTime'];
+            var endTime=params['endTime'];
+            var seckillId=params['seckillId'];
+            $.get(seckill.URL.now(),{},function (result) {
+                if(result && result['success']){
+                    var nowTime = result['data'];
+                    //时间判断
+                    seckill.countdown(seckillId,nowTime,startTime,endTime);
+                }else {
+                    console.log('result:'+result);
+                }
+            })
         }
     },
     validatePhone:function (phone) {
@@ -39,6 +52,19 @@ var seckill={
             return true;
         }else {
             return false;
+        }
+    },
+    countdown:function (seckillId,nowTime,startTime,endTime) {
+        var seckillBox = $('#seckill-box');
+        if(nowTime > endTime){
+            //秒杀结束
+            seckillBox.html('秒杀结束');
+        }else if (nowTime <startTime){
+            //秒杀未开始
+            var killTime = new Date(startTime + 1000);
+            seckillBox.countdown();
+        }else {
+
         }
     }
 }
